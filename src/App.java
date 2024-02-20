16deeba5a4867a75ff0b3759a508bc939d068faa
Génesis Mauries <exodo_54@hotmail.com>
2024-02-20 16:05:56 -0600

    add second version hello

--- src/App.java
+++ src/App.java
@@ -1,5 +1,18 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+// public class App {
+//     public static void main(String[] args) throws Exception {
+//         System.out.println("Hello, World!");
+//     }
+// }
+
+class SampleClass {
+    void output() {
+        System.out.println("Hello World!");
+    }
+}
+
+class Main {
+    public static void main(String args[]) {
+        SampleClass obj = new SampleClass();
+        obj.output();
     }
 }
