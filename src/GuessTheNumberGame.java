de4b99fc1e5de9eb119beca4a6f83ccd82225077
Génesis Mauries <exodo_54@hotmail.com>
2024-02-22 12:19:17 -0600

    no funciona tiene errores para ejecutar el metodo startGame

--- src/GuessTheNumberGame.java
+++ src/GuessTheNumberGame.java
@@ -7,13 +7,28 @@ public class GuessTheNumberGame {
     // int number, i;
 
     public static void main(String [] args){
-        System.out.print("Ingresa tu nombre: ");
+        System.out.print("Ingresa tu nombre: ⌨️ ");
         Scanner scanner = new Scanner(System.in);
         String userName = scanner.nextLine();
         System.out.println(userName + " Adivina el número");
         System.out.println("Elige un número entre 1 y 10,");
-
-        int numberRandom = random.nextInt(10);
+        HumanPlayer humanPlayer = new HumanPlayer(userName);
+        ComputerPlayer computerPlayer = new ComputerPlayer("Oponente virtual");
+        startGame(humanPlayer, computerPlayer);
+        private static Player startGame(humanPlayer, computerPlayer) {
+            for(i=1;;i++){
+                Scanner entrada = new Scanner(System.in);
+                targetNumber = entrada.nextInt();
+                if(numberRandom < targetNumber){
+                    System.out.println("⬆️ Muy alto, intenta nuevamente");
+                }else if(numberRandom > targetNumber){
+                    System.out.println("⬆⬇️ Muy bajo, intenta nuevamente");
+                } else if (numberRandom == targetNumber) {
+                    System.out.println("🥳 Correcto acertaste en " + i + " intentos");
+                }
+            }
+        }
+        /*int numberRandom = random.nextInt(10);
         int targetNumber;
         int number, i;
         for(i=1;;i++){
@@ -26,7 +41,7 @@ public class GuessTheNumberGame {
             } else if (numberRandom == targetNumber) {
                 System.out.println("🥳 Correcto acertaste en " + i + " intentos");
             }
-        }
+        }*/
 
     }
 }
