fe7607853f933bc16818805d081f6e8ce4719f5f
Génesis Mauries <exodo_54@hotmail.com>
2024-02-29 14:58:21 -0600

    add humanPlayer test

--- src/test/java/HumanPlayerTest.java
+++ src/test/java/HumanPlayerTest.java
@@ -1,20 +1,24 @@
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayInputStream;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class HumanPlayerTest {
+    String playerName = "Test";
+    HumanPlayer humanPlayer = new HumanPlayer(playerName);
     @Test
     public void playerShouldHaveCorrectName() {
-        String playerName = "Test";
-        HumanPlayer humanPlayer = new HumanPlayer(playerName);
         assertEquals(playerName, humanPlayer.getName(), "Debería llamar a la jugadora con el nombre correcto");
     }
-    // Pa´ revisar 👀
+
     @Test
     public void makeGuessShouldReturnValidNumber() {
-        HumanPlayer humanPlayer = new HumanPlayer("Test");
-        int guess = 15;
-        assertFalse(false, "Debería ser un numero entre 0 y 10");
+        String input = "5";
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+        int guess = humanPlayer.makeGuess();
+        assertTrue(guess >= 1 && guess <= 10, "Guess should be between 1 and 10");
+        System.setIn(System.in);
     }
 
 }
