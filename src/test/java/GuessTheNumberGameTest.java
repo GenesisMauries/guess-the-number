b042e247ce2cd4f715065f4b0dff145a1c643382
Génesis Mauries <exodo_54@hotmail.com>
2024-03-04 14:12:28 -0600

    add second test

--- src/test/java/GuessTheNumberGameTest.java
+++ src/test/java/GuessTheNumberGameTest.java
@@ -1,13 +1,13 @@
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
-import static org.mockito.Mockito.times;
+
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.*;
 
 public class GuessTheNumberGameTest {
 @Test
 public void testGameWithMockedPlayers() {
-    int targetNumber = 3;
     HumanPlayer mockHumanPlayer = Mockito.mock(HumanPlayer.class);
     ComputerPlayer mockComputerPlayer = Mockito.mock(ComputerPlayer.class);
     when(mockHumanPlayer.getName()).thenReturn("JugadorMock");
@@ -18,9 +18,26 @@ public void testGameWithMockedPlayers() {
 
     game.startGame();
 
-    Mockito.verify(mockHumanPlayer, times(3)).makeGuess();
-    Mockito.verify(mockComputerPlayer, times(3)).makeGuess();
+    verify(mockHumanPlayer, times(3)).makeGuess();
+    verify(mockComputerPlayer, times(3)).makeGuess();
     assertFalse(game.checkGuess(mockHumanPlayer));
+    assertFalse(game.checkGuess(mockComputerPlayer));
 }
+    @Test
+    public void testGameWithMockedPlayersWinner() {
+        HumanPlayer mockHumanPlayer = Mockito.mock(HumanPlayer.class);
+        ComputerPlayer mockComputerPlayer = Mockito.mock(ComputerPlayer.class);
+        when(mockHumanPlayer.getName()).thenReturn("JugadorMock");
+        when(mockHumanPlayer.makeGuess()).thenReturn(3);
+        when(mockComputerPlayer.getName()).thenReturn("OponenteMock");
+        when(mockComputerPlayer.makeGuess()).thenReturn(7);
+
+        GuessTheNumberGame game = new GuessTheNumberGame(mockHumanPlayer, mockComputerPlayer);
+        game.startGame();
+
+        verify(mockHumanPlayer, times(3)).makeGuess();
+        verify(mockComputerPlayer, times(3)).makeGuess();
+        assertTrue(game.checkGuess(mockHumanPlayer));
+    }
 
 }
