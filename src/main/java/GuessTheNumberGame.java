13c34366e0ee17ab026f66f375c5c1089ac43ac9
Génesis Mauries <exodo_54@hotmail.com>
2024-03-04 12:37:05 -0600

    refactor for test

--- src/main/java/GuessTheNumberGame.java
+++ src/main/java/GuessTheNumberGame.java
@@ -5,39 +5,45 @@ public class GuessTheNumberGame {
     private static final Random random = new Random();
     private static int targetNumber;
 
-    public static void main(String [] args){
+    private final Player humanPlayer;
+    private final Player virtualOpponent;
+
+    public GuessTheNumberGame(Player humanPlayer, Player virtualOpponent) {
+        this.humanPlayer = humanPlayer;
+        this.virtualOpponent = virtualOpponent;
+    }
+
+    public static void main(String[] args) {
         System.out.print("Ingresa tu nombre: ");
         Scanner scanner = new Scanner(System.in);
         String userName = scanner.nextLine();
-        startGame(userName);
+        GuessTheNumberGame game = new GuessTheNumberGame(new HumanPlayer(userName), new ComputerPlayer("Oponente virtual"));
+        game.startGame();
     }
 
-    private static void startGame(String userName) {
-        HumanPlayer humanPlayer = new HumanPlayer(userName);
-        ComputerPlayer virtualOpponent = new ComputerPlayer("Oponente virtual");
+    public void startGame() {
         targetNumber = random.nextInt(10);
 
         int i;
         for (i = 1; i <= 6; i++) {
             Player currentPlayer = i % 2 == 1 ? humanPlayer : virtualOpponent;
-            if(checkGuess(currentPlayer)){
+            if (checkGuess(currentPlayer)) {
                 break;
             }
         }
     }
-    private static boolean checkGuess(Player player) {
+
+    public boolean checkGuess(Player player) {
         System.out.println("Turno de: " + player);
         int guess = player.makeGuess();
-        if(guess == targetNumber) {
+        if (guess == targetNumber) {
             System.out.println(" 🥳 Correcto " + player.getName() + ", acertaste el numero es: " + targetNumber);
             return true;
-        }
-        else if (guess > targetNumber) {
+        } else if (guess > targetNumber) {
             System.out.println(guess + " ⬆️ Muy alto, intenta nuevamente");
-        } else if(guess < targetNumber) {
+        } else if (guess < targetNumber) {
             System.out.println(guess + " ⬇️ Muy bajo, intenta nuevamente");
         }
         return false;
     }
-
 }
